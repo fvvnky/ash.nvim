1d8cb682968bf1056de440abbebb1553ee447299
peperoni <brucewayne0002@proton.me>
2025-05-20 02:19:10 +0100

    fix color

--- lua/ash/palette.lua
+++ lua/ash/palette.lua
@@ -1,29 +1,29 @@
 return {
     none = "NONE",
-    text      = "#8c8c8c", -- medium-light gray (main text)
-    subtext1  = "#a0a0a0", -- lighter gray (secondary text)
-    subtext0  = "#787878", -- darker than text, but lighter than overlays
+    text = "#dddddd", -- medium-light gray (main text)
+    subtext1 = "#a0a0a0", -- lighter gray (secondary text)
+    subtext0 = "#787878", -- darker than text, but lighter than overlays
 
     overlay2 = "#8a8a8a", -- medium-light neutral gray
     overlay1 = "#707070", -- medium gray
     overlay0 = "#606060", -- darker gray
     surface2 = "#4a4a4a", -- charcoal gray
     surface1 = "#2f2f2f", -- near-black, neutral
-    surface0 = "#1a1a1a", 
+    surface0 = "#1a1aff",
     base = "#000000",
     mantle = "#020203",
     crust = "#050505",
-    seafoam  = "#aabbb5", -- dulled sea green
-    rose     = "#b3989a", -- muted dusty pink
-    ember    = "#b29b89", -- dull amber
-    storm    = "#959da6", -- soft bluish gray
-    crimson  = "#a37b80", -- soft desaturated red
-    rust     = "#a78678", -- earthy brownish
-    frost    = "#a2abad", -- light steel gray
-    sage     = "#a4afa4", -- dusty green-gray
-    tide     = "#8fa2a7", -- muted sea blue
-    slate    = "#878892", -- desaturated cool gray
-    drift    = "#9aa6a8", -- pale blue-gray
+    seafoam = "#aabbb5", -- dulled sea green
+    rose = "#b3989a", -- muted dusty pink
+    ember = "#b29b89", -- dull amber
+    storm = "#959da6", -- soft bluish gray
+    crimson = "#a37b80", -- soft desaturated red
+    rust = "#a78678", -- earthy brownish
+    frost = "#a2abad", -- light steel gray
+    sage = "#a4afa4", -- dusty green-gray
+    tide = "#8fa2a7", -- muted sea blue
+    slate = "#878892", -- desaturated cool gray
+    drift = "#9aa6a8", -- pale blue-gray
     charcoal = "#747680", -- softened dark gray
-    fog      = "#9c9ca7", -- cool mist gray
+    fog = "#9c9ca7", -- cool mist gray
 }
